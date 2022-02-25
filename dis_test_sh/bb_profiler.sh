331b181e9305d20c52124a9961e9a7e7c368139c
Fannxy <fanxy20@mails.tsinghua.edu.cn>
2022-02-25 07:11:20 +0000

    update distributed shell

--- dis_test_sh/bb_profiler.sh
+++ dis_test_sh/bb_profiler.sh
@@ -1,7 +1,7 @@
 echo "==== Test basic building blocks ===="
 mode=$1
 
-logFolder=./MP-SPDZ/Record/dis_basic_profiler/
+logFolder=~/MP-SPDZ/Record/dis_basic_profiler/
 logFile=${logFolder}profiler
 logTmp=${logFolder}tmp
 sourceFile=bb_profiler
@@ -15,16 +15,24 @@ protocol=./replicated-ring-party.x
 echo -e "Protocol - ${protocol}" >> ${logFile};
 
 if [ ${mode} == "compile" ]; then
-    compile_command_main="cd ./MP-SPDZ/; ./compile.py -R 256 ${sourceFile} ring >> ${compileLog};"
-    ${compile_command} &
+    echo "compile here" ;
+    # compile_command_main="cd ./MP-SPDZ/ ./compile.py -R 256 ${sourceFile} ring >> ${compileLog};"
+
+    cd ./MP-SPDZ/
+    ./compile.py -R 256 ${sourceFile} ring >> ${compileLog} & 
+    cd ..
+
+    compile_command="cd ./MP-SPDZ/; ./compile.py -R 256 ${sourceFile} ring;"
     ssh mp-spdz131 ${compile_command} & 
     ssh mp-spdz132 ${compile_command}
 fi
 wait;
 
 if [ ${mode} == "exec" ]; then
-    cd ./MP-SPDZ; ./dis_test_sh/exec.sh 0 ${protocol} ${sourceFile}-ring ${logFolder} & 
-    ssh mp-spdz131 "cd ./MP-SPDZ/; ./dis_test_sh/exec.sh 1 ${protocol} ${sourceFile}-ring ${logFolder}" &
-    ssh mp-spdz132 "cd ./MP-SPDZ/; ./dis_test_sh/exec.sh 2 ${protocol} ${sourceFile}-ring ${logFolder}"
+    cd ./MP-SPDZ 
+    ./basic_dis/dis_test_sh/exec.sh 0 ${protocol} ${sourceFile}-ring ${logFolder} & 
+    cd ..
+    ssh mp-spdz131 "cd ./MP-SPDZ/; ./dis_test_sh/basic_dis/exec.sh 1 ${protocol} ${sourceFile}-ring ${logFolder}" &
+    ssh mp-spdz132 "cd ./MP-SPDZ/; ./dis_test_sh/basic_dis/exec.sh 2 ${protocol} ${sourceFile}-ring ${logFolder}"
 fi
 wait;
