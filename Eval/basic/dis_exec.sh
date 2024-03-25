c03f855e8669300ba59e7ece02849491b932d2c0
Fannxy <fanxy20@mails.tsinghua.edu.cn>
2024-03-25 09:41:00 +0000

    update spdz origional benchmark

--- Eval/basic/dis_exec.sh
+++ Eval/basic/dis_exec.sh
@@ -5,6 +5,6 @@ logTmp=${logFolder}tmp.txt
 echo -e "\n\nTest $1 using protocol $2 \n" >> ${logFile}
 
 ./Eval/basic/dis_exec_unit.sh ${task} ${protocol} ${logFolder} ${logFile} 0 &
-ssh spdz1 "cd ./MP-SPDZ/; ./Eval/basic/dis_exec_unit.sh ${task} ${protocol} ${logFolder} ${logFile} 1" &
-ssh spdz2 "cd ./MP-SPDZ/; ./Eval/basic/dis_exec_unit.sh ${task} ${protocol} ${logFolder} ${logFile} 2" &
+ssh spdz1 "cd ./MP-SPDZ/; ./Eval/basic/dis_exec_unit.sh ${task} ${protocol} ${logFolder} ${logFile} 1" >> /dev/null &
+ssh spdz2 "cd ./MP-SPDZ/; ./Eval/basic/dis_exec_unit.sh ${task} ${protocol} ${logFolder} ${logFile} 2" >> /dev/null &
 wait;
--- Eval/spdz_benchmark.sh
+++ Eval/spdz_benchmark.sh
@@ -4,6 +4,9 @@ clogFile=${clogFolder}compile_log
 sourceFile=benchmark_spdz_origional
 protocol=replicated-ring-party.x
 
+BuildFlag=False
+CompileOnly=True
+
 if [ ! -d ${logFolder} ]; then
     mkdir ${logFolder};
 fi
@@ -12,33 +15,54 @@ if [ ! -d ${clogFolder} ]; then
     mkdir ${clogFolder};
 fi
 
-N_list=(1 1)
-M_list=(1024 16384)
-parallel_list=(16 64)
+# N_list=(1 1 1 1)
+# M_list=(16777216 268435456 1073741824)
+# parallel_list=(1 1 1 1)
+
+N_list=(1)
+M_list=(16777216)
+parallel_list=(1)
 param_len=${#M_list[@]}
 task_list=(cipher_index)
 REPEAT=1
+
+if [ $BuildFlag = True ]; then
+    
+    cd ..
+    scp -r ./MP-SPDZ/CONFIG.mine spdz1:~/MP-SPDZ/ &
+    scp -r ./MP-SPDZ/CONFIG.mine spdz2:~/MP-SPDZ
+    wait;
+    cd ./MP-SPDZ/;
+
+    make -j 8 replicated-ring-party.x &
+    ssh spdz1 "cd ./MP-SPDZ/; make -j 8 replicated-ring-party.x" &
+    ssh spdz2 "cd ./MP-SPDZ/; make -j 8 replicated-ring-party.x" &
+    wait;
+
+fi
 
 # compile the baseline functions.
 for ((i=0; i<param_len; i++)); do
     M=${M_list[i]}; N=${N_list[i]}; parallel=${parallel_list[i]};
     for task in ${task_list[*]}; do
         clog=${clogFile}-${task}-${N}-${M}-${REPEAT}-${parallel};
-        python compile.py -l -R 64 -Z 3 ${sourceFile} ${task} $N $M $REPEAT $parallel > ${clog} &
+        bc_file=./Programs/Schedules/${sourceFile}-${task}-${N}-${M}-${REPEAT}-${parallel}.sch 
+        if [ -f ${bc_file} ]; then
+            continue
+        fi
+        python compile.py -R 256 -l ${sourceFile} ${task} $N $M $REPEAT $parallel > ${clog} 2>&1 &
         wait;
     done;
 done;
+
+if [ $CompileOnle = True ]; then
+    exit;
+fi
 
 # synchronized with other machines.
 cd ..
 scp -r ./MP-SPDZ/Programs spdz1:~/MP-SPDZ/ &
 scp -r ./MP-SPDZ/Programs spdz2:~/MP-SPDZ
-wait;
-scp -r ./MP-SPDZ/*.x spdz1:~/MP-SPDZ/  &
-scp -r ./MP-SPDZ/*.x spdz2:~/MP-SPDZ/
-wait;
-scp -r ./MP-SPDZ/*.so spdz1:~/MP-SPDZ/  &
-scp -r ./MP-SPDZ/*.so spdz2:~/MP-SPDZ/
 wait;
 cd ./MP-SPDZ/;
 
@@ -53,7 +77,6 @@ for ((i=0; i<param_len; i++)); do
         recordFolder=${logFolder}Record_${task}
         logFile=${recordFolder}/Record-bso-baseline
         if [ ! -d ${recordFolder} ]; then
-            echo "in this line!"
             mkdir ${recordFolder};
         fi
         elog=${logFile}-${task}-n=${N}-m=${M}-k=1-R=${REPEAT}-c=${parallel};
@@ -61,13 +84,3 @@ for ((i=0; i<param_len; i++)); do
         wait;
     done;
 done;
-
-# for M in ${M_list[*]}; do
-#     for task in ${task_list[*]}; do
-#         for parallel in ${parallel_list[*]}; do
-#             clog=${clogFile}-${task}-${N}-${M}-${REPEAT}-${parallel};
-#             python compile.py -l -R 64 -Z 3 ${sourceFile} ${task} $N $M $REPEAT $parallel > ${clog} &
-#             wait;
-#         done;
-#     done;
-# done;
