b00b4fb5a67fa13728fcfae9380f41f99f81973d
Xiang Li <lixiang20@mails.tsinghua.edu.cn>
2024-06-22 03:35:18 +0000

    modify prepare_env_3.sh

--- build_env_for_aliyun/prepare_env_3.sh
+++ build_env_for_aliyun/prepare_env_3.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+mkdir -p /Programs
+mkdir -p /Player-Data
+
 # need to change the HOST if necessary
 cp MP-SPDZ/HOST /HOST
 # need to compile all programs before the copy
@@ -11,7 +14,7 @@ cp -r MP-SPDZ/Player-Data /
 cd MP-SPDZ
 cp ../demos/hello_c/hello.yaml .
 sed -i "s/hello_world/hemi-offline.x/g" hello.yaml
-for filename in replicated-ring-party.x replicated-field-party.x ps-rep-ring-party.x ps-rep-field-party.x semi2k-party.x semi-party.x spdz2k-party.x mascot-party.x semi-offline.x semi2k-offline.x
+for filename in replicated-ring-party.x replicated-field-party.x ps-rep-ring-party.x ps-rep-field-party.x shamir-party.x malicious-shamir-party.x semi2k-party.x semi-party.x spdz2k-party.x mascot-party.x hemi-party.x lowgear-party.x soho-party.x highgear-party.x semi-offline.x semi2k-offline.x 
 do
         sed -i -e '$a\ \ \ \ \ \ \ \ - ..\/'"$filename"'' hello.yaml
 done
@@ -21,13 +24,13 @@ copy_bom -f ../hello.yaml --root image --include-dir /opt/occlum/etc/template
 cp ../HOST image/
 cp -r ../Programs image/
 cp -r ../Player-Data image/
-sed -i "s/\"kernel_space_heap_size\": \"32MB\",/\"kernel_space_heap_size\": \"256MB\",/g" Occlum.json
-sed -i "s/\"kernel_space_stack_size\": \"1MB\",/\"kernel_space_stack_size\": \"16MB\",/g" Occlum.json
+sed -i "s/\"kernel_space_heap_size\": \"32MB\",/\"kernel_space_heap_size\": \"128MB\",/g" Occlum.json
+sed -i "s/\"kernel_space_stack_size\": \"1MB\",/\"kernel_space_stack_size\": \"32MB\",/g" Occlum.json
 sed -i "s/\"user_space_size\": \"300MB\",/\"user_space_size\": \"16GB\",/g" Occlum.json
-sed -i "s/\"max_num_of_threads\": 32/\"max_num_of_threads\": 128/g" Occlum.json
-sed -i "s/\"default_stack_size\": \"4MB\",/\"default_stack_size\": \"64MB\",/g" Occlum.json
-sed -i "s/\"default_heap_size\": \"32MB\",/\"default_heap_size\": \"2GB\",/g" Occlum.json
-sed -i "s/\"default_mmap_size\": \"100MB\"/\"default_mmap_size\": \"8GB\"/g" Occlum.json
+sed -i "s/\"max_num_of_threads\": 32/\"max_num_of_threads\": 1024/g" Occlum.json
+sed -i "s/\"default_stack_size\": \"4MB\",/\"default_stack_size\": \"512KB\",/g" Occlum.json
+sed -i "s/\"default_heap_size\": \"32MB\",/\"default_heap_size\": \"16MB\",/g" Occlum.json
+sed -i "s/\"default_mmap_size\": \"100MB\"/\"default_mmap_size\": \"32MB\"/g" Occlum.json
 sed -i "s/\"debuggable\": true,/\"debuggable\": false,/g" Occlum.json
 echo -e "nameserver 8.8.8.8\nnameserver 8.8.4.4" > image/etc/resolv.conf
 echo -e "hosts: files dns" > image/etc/nsswitch.conf
