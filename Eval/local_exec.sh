c2814cb5b69a93823484949799a2d2f1a80f027d
Fannxy <fxy23@mails.tsinghua.edu.cn>
2025-10-22 15:55:05 +0800

    add local_exec

--- Eval/local_exec.sh
+++ Eval/local_exec.sh
@@ -6,7 +6,7 @@ logTmp=/dev/null
 
 echo -e "\n\nTest $1 using protocol $2 \n" >> ${logFile}
 
-# ./${protocol} -p 0 ${task} >> ${logFile} 2>&1 & ./${protocol} -p 1 ${task} >> ${logTmp} & ./${protocol} -p 2 ${task} >> ${logTmp} &
+# for performance test, ssh to the virtual hosts and run the programs!
 ./${protocol} -p 0 ${task} -v >> ${logFile} 2>&1 & ./${protocol} -p 1 -v ${task} >> ${logTmp} & ./${protocol} -p 2 ${task} -v >> ${logTmp} &
-wait
+wait;
 echo -e "Success" >> ${logFile}
